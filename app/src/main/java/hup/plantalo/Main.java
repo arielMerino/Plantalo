e003f6656534350ad879fd2f36765286587cb5f2
ariel <arielMerino@users.noreply.github.com>
2016-02-17 19:27:53 -0300

    Vista de un cultivo

--- app/src/main/java/hup/plantalo/Main.java
+++ app/src/main/java/hup/plantalo/Main.java
@@ -1,13 +1,28 @@
 package hup.plantalo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends AppCompatActivity {
+
+    Button botonCultivo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        botonCultivo = (Button) findViewById(R.id.cultivo);
+
+        botonCultivo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Cultivo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
