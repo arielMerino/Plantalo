b73c2880cb86a2292519c4d4035d9e05a037b62a
eliasplease <elias.sobarzo@usach.cl>
2016-02-22 16:05:24 -0300

    Se agregan tabs en vista cultivos

---
+++ app/src/main/java/hup/plantalo/TabBarActivity.java
@@ -0,0 +1,29 @@
+package hup.plantalo;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class TabBarActivity extends TabActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cultivo);
+
+        TabHost tabHost = getTabHost();
+        TabHost.TabSpec spec;
+        Intent intent;
+
+        intent = new Intent().setClass(this, Cultivo.class);
+        spec = tabHost.newTabSpec("First").setIndicator("Comentarios").setContent(intent);
+        tabHost.addTab(spec);
+
+        intent = new Intent().setClass(this, Main.class);
+        spec = tabHost.newTabSpec("Second").setIndicator("Tips").setContent(intent);
+        tabHost.addTab(spec);
+
+        intent = new Intent().setClass(this, Cultivo.class);
+        spec = tabHost.newTabSpec("Third").setIndicator("Consejos").setContent(intent);
+        tabHost.addTab(spec);
+    }
+}
