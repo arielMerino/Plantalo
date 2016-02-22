d4008af917abf60cd3ff54089ac63333c0e9e990
eliasplease <elias.sobarzo@usach.cl>
2016-02-22 16:02:14 -0300

    Se agrega tabs en vista cultivos

--- app/src/main/java/hup/plantalo/Main.java
+++ app/src/main/java/hup/plantalo/Main.java
@@ -20,7 +20,7 @@ public class Main extends AppCompatActivity {
         botonCultivo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Cultivo.class);
+                Intent intent = new Intent(getApplicationContext(), TabBarActivity.class);
                 startActivity(intent);
             }
         });
